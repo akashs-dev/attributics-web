a6caf74d83432a5c8447f510c952981030707d5b
Manaswa S <mnswamain@gmail.com>
2026-03-12 22:48:24 +0530

    chore bugs

--- backend/index.js
+++ backend/index.js
@@ -504,6 +504,7 @@ const smtpMailer = {
       host: process.env.SMTP_HOST,
       port: Number(process.env.SMTP_PORT) || 587,
       secure: process.env.SMTP_SECURE === "true", // true for 465, false for 587
+      family: 4,
       auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS,
